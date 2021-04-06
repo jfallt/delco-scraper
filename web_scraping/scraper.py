38c3fb0b5c0bc4beda376c7cb2f1c8bf7ecf3b18
jfallt <fallt.jared@gmail.com>
2021-04-06 13:35:49 -0400

    put parcel function in scraper.py

--- web_scraping/scraper.py
+++ web_scraping/scraper.py
@@ -204,3 +204,42 @@ def sales_history_by_year_batch(year_start, year_end):
     runtime = stop - start
     print('Total runtime {runtime} seconds'.format(
         runtime=runtime))
+
+def delco_scraper_parcel(parcel_id):
+    """
+    Use the parcel id to search parcel details on http://delcorealestate.co.delaware.pa.us/
+    """
+
+    # Define table fields and format
+    fields = ['ParcelID', 'TaxMapID', 'OwnerName', 'PropertyAddress',
+              'SalesDate', 'SalesAmount', 'LandUseDescription']
+    date_format = '%m/%d/%Y'
+
+    # Required urls
+    url_home = 'http://delcorealestate.co.delaware.pa.us'
+    url_search = 'http://delcorealestate.co.delaware.pa.us/pt/search/commonsearch.aspx?mode=parid'
+
+    # Navigate to search page
+    driver = get_selenium_res()
+    driver.get(url_home)
+    driver.get(url_search)
+
+    # Trying to go to the url_search page results in a disclaimer page, click agree
+    driver.find_element_by_id('btAgree').click()
+    
+    # Submit parcel id
+    driver.find_element_by_id('inpParid').send_keys(parcel_id)
+    driver.find_element_by_id('btSearch').click()
+
+    # Scrape data
+    data_headers = driver.find_elements_by_class_name('DataletSideHeading')
+    data = driver.find_elements_by_class_name('DataletData')
+    
+    data_list = []
+    data_headers_list = []
+    [data_list.append(entry.text) for entry in data]
+    [data_headers_list.append(entry.text) for entry in data_headers]
+    zipped_list = zip(data_headers_list, data_list)
+    parcel_details = list(zipped_list)
+    print('Complete!')
+    driver.quit()
