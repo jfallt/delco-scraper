2953988e59370015a2f3554e0ebe6e11a072edee
jfallt <fallt.jared@gmail.com>
2021-05-04 12:59:56 -0400

    formatting, add insert and missing data queries

--- queries.py
+++ queries.py
@@ -1,5 +1,4 @@
-insert_residential_details = """
-INSERT INTO stg_parcel_residential_details(
+insert_residential_details = """INSERT INTO stg_parcel_residential_details(
     parcel_id,
     card,
     class,
@@ -60,3 +59,38 @@ INSERT INTO stg_parcel_residential_details(
             '{family_room}',
             '{living_units}'
             )"""
+
+
+insert_parcel_site_details = """INSERT INTO stg_parcel_site_details(
+    parcel_id,
+    site_location,
+    legal_description,
+    municipality,
+    school_district,
+    property_type) 
+    VALUES (
+            '{parcel_id}',
+            '{site_location}',
+            '{legal_description}',
+            '{municipality}',
+            '{school_district}',
+            '{property_type}'
+            )"""
+
+get_missing_data = {
+    'residential_details': """SELECT P.PARCEL_ID
+    FROM DIM_PARCEL P
+    LEFT JOIN dim_parcel_residential_details FPD ON FPD.ID = P.ID
+    GROUP BY P.PARCEL_ID
+    HAVING COUNT(FPD.ID) = 0
+    ORDER BY P.PARCEL_ID DESC""",
+    'parcel_site_details': """call public.sp_update_dim_parcel_all();
+    truncate stg_parcel_site_details;
+    SELECT P.PARCEL_ID
+    FROM DIM_PARCEL P
+	LEFT JOIN (select distinct parcel_id from stg_parcel_site_details) sd
+	on sd.parcel_id = p.parcel_id
+    WHERE municipality is null
+	and sd.parcel_id is null
+    ORDER BY P.PARCEL_ID DESC;"""
+}
